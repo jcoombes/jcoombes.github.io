68b24c538d41933f28a455a2e1097842b61ca2c7
Jamie <coombes.jd@gmail.com>
2018-11-17 23:53:46 +0000

    import json into javascript

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ getJSON('gallery2.json',  function(err, data) {
         console.error(err);
     } else {
 
-        var text = `Date: ${data["images"]["guitar"]}
+        var text = `Date: ${data.images}
 Time: ${data.time}
 Unix time: ${data.milliseconds_since_epoch}`
 
