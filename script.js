5ab6631f33e34cd93671065140e617de4ce4667e
Jamie <coombes.jd@gmail.com>
2018-11-18 00:29:30 +0000

    import json into javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var getJSON = function(url, callback) {
 
     var xhr = new XMLHttpRequest();
@@ -10,29 +9,35 @@ var getJSON = function(url, callback) {
         var status = xhr.status;
 
         if (status == 200) {
-            callback(null, xhr.response);
+            callback(null, xhr.response, main);
         } else {
-            callback(status);
+            callback(status, main);
         }
     };
 
     xhr.send();
 };
 
-var looking = getJSON('gallery2.json',  function(err, data) {
+var looking = getJSON('gallery2.json',  function(err, data, main) {
 
     if (err != null) {
         console.error(err);
     } else {
 
         var gallery = data;
-        console.log('gallery: ',gallery);
-       console.log('data: ',data);
-        return gallery;
+        // I have to put all my code inside this asynchronous block. :(
+        main("hello");
+
+
     }
 });
 
 console.log(looking);
+
+
+var main = function (foo) {
+  alert(foo);
+}
 
 function times_liked() {
 
