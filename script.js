e5ab0ef995d3367b5e0c603233bef55a44c1f92d
Jamie <coombes.jd@gmail.com>
2018-11-18 01:45:26 +0000

    scoping

--- script.js
+++ script.js
@@ -64,13 +64,13 @@ function times_liked(gallery, tag_list, user_likes, user_dislikes) {
 
     for (var like in user_likes) {
       console.log("likes",user_likes[like],
-      gallery["images"][user_likes[like]]["picture_attributes"][tag_list[tag]]
+      gallery[0]["images"][user_likes[like]]["picture_attributes"][tag_list[tag]]
       );
     };
 
     for (var dislike in user_dislikes) {
       console.log("dislikes",user_dislikes[dislike],
-      gallery["images"][user_dislikes[dislike]]["picture_attributes"][tag_list[tag]]
+      gallery[0]["images"][user_dislikes[dislike]]["picture_attributes"][tag_list[tag]]
       );
     };
 
