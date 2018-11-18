85d3aa8d737074d47b260cbf4e878149de68c95e
Jamie <coombes.jd@gmail.com>
2018-11-18 02:06:33 +0000

    times_liked

--- script.js
+++ script.js
@@ -64,15 +64,18 @@ function times_liked(gallery, tag_list, user_likes, user_dislikes) {
     for (var like in user_likes) {
       if (
       gallery[0]["images"][user_likes[like]]["picture_attributes"][tag_list[tag]] === true
-    );
+    ) {
       user_prefs[tag_list[tag]] = user_prefs[tag_list[tag]] + 1;
+    }
     };
 
     for (var dislike in user_dislikes) {
       if (
       gallery[0]["images"][user_dislikes[dislike]]["picture_attributes"][tag_list[tag]] === true
-      );
-        user_prefs[tag_list[tag]] = user_prefs[tag_list[tag]] - 1;
+    ) {
+        user_prefs[tag_list[tag]] = user_prefs[tag_list[tag]] - 1
+    };
+
     };
   };
   console.log(user_prefs);
